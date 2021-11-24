b805b48d66a379e5ecb93b1034b59a291083a72b
Yohann Legrand <yohann.legrand-ext@believe.com>
2021-11-24 10:47:01 +0100

    ability to delete your own jokes

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,25 +1,58 @@
+import type { LoaderFunction, ActionFunction } from "remix"
+import { Link, useCatch, useLoaderData, useParams, redirect } from "remix"
 import { Joke } from "@prisma/client"
-import { LoaderFunction } from "@remix-run/server-runtime"
-import { Link, useCatch, useLoaderData, useParams } from "remix"
+
 import { db } from "~/utils/db.server"
+import { getUserId, requireUserId } from "~/utils/session.server"
 
 type LoaderData = {
   joke: Joke
+  isOwner: boolean
 }
-export const loader: LoaderFunction = async ({ params }) => {
+export const loader: LoaderFunction = async ({ request, params }) => {
   const joke = await db.joke.findUnique({ where: { id: params.jokeId } })
   if (!joke) throw new Response("Joke not found", { status: 404 })
-  const data: LoaderData = { joke }
+  const userId = await getUserId(request)
+  const data: LoaderData = { joke, isOwner: joke.jokesterId === userId }
   return data
 }
 
+export const action: ActionFunction = async ({ request, params }) => {
+  const form = await request.formData()
+  if (form.get("_method") === "delete") {
+    const userId = await requireUserId(request)
+    const joke = await db.joke.findUnique({ where: { id: params.jokeId } })
+    if (!joke) {
+      throw new Response("Can't delete a joke that does not exist", {
+        status: 404,
+      })
+    }
+    if (joke.jokesterId !== userId) {
+      throw new Response("Nice try. That's not your joke", { status: 401 })
+    }
+    await db.joke.delete({ where: { id: params.jokeId } })
+    return redirect("/jokes")
+  }
+}
+
 export default function JokeRoute() {
-  const { joke } = useLoaderData<LoaderData>()
+  const { joke, isOwner } = useLoaderData<LoaderData>()
   return (
     <div>
       <p>Here's your hilarious joke:</p>
       <p>{joke.content}</p>
       <Link to={`/jokes/${joke.id}`}>"{joke.name}" Permalink</Link>
+
+      {isOwner && (
+        <div className="jokes-footer">
+          <form method="post">
+            <input type="hidden" name="_method" value="delete" />
+            <button type="submit" className="button">
+              Delete
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   )
 }
@@ -37,11 +70,26 @@ export function CatchBoundary() {
   let caught = useCatch()
   const { jokeId } = useParams()
 
-  if (caught.status === 404) {
-    return (
-      <div className="error-container">Huh? What the heck is "{jokeId}" ?</div>
-    )
+  switch (caught.status) {
+    case 404:
+      return (
+        <div className="error-container">
+          Huh? What the heck is "{jokeId}" ?
+        </div>
+      )
+    case 405:
+      return (
+        <div className="error-container">
+          Huh? What are you trying to do here?
+        </div>
+      )
+    case 401:
+      return (
+        <div className="error-container">
+          You don't have permission to do that.
+        </div>
+      )
+    default:
+      throw new Error(`Unhandled error: ${caught.status}`)
   }
-
-  throw new Error(`Unhandled error: ${caught.status}`)
 }
