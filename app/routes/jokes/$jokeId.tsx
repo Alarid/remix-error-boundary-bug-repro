eb17114afc9ef5a61a8492ec9d09b44587b5550e
Yohann Legrand <yohann.legrand-ext@believe.com>
2021-11-24 09:49:17 +0100

    unexpected errors

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,6 +1,6 @@
 import { Joke } from "@prisma/client"
 import { LoaderFunction } from "@remix-run/server-runtime"
-import { Link, useLoaderData } from "remix"
+import { Link, useLoaderData, useParams } from "remix"
 import { db } from "~/utils/db.server"
 
 type LoaderData = {
@@ -15,12 +15,20 @@ export const loader: LoaderFunction = async ({ params }) => {
 
 export default function JokeRoute() {
   const { joke } = useLoaderData<LoaderData>()
-
   return (
     <div>
       <p>Here's your hilarious joke:</p>
       <p>{joke.content}</p>
       <Link to={`/jokes/${joke.id}`}>"{joke.name}" Permalink</Link>
+    </div>
+  )
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  const { jokeId } = useParams()
+  return (
+    <div className="error-container">
+      There was an error loading joke #{jokeId}, sorry.
     </div>
   )
 }
